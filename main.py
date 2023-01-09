393e1bb470e718396eef4c36a54628119ccef463
cyberbishop <alabiavin@gmail.com>
2023-01-09 11:36:09 +0100

    delete 2 files and update 2 files

--- main.py
+++ main.py
@@ -96,8 +96,14 @@ def subdomain_enumeration():
 
 
 def massdns_resolution():
+  # Create a new folder for massdns
+  if not os.path.exists('massdns'):
+    os.makedirs('massdns')
+
+  # Change the working directory to the massdns folder
+  os.chdir('massdns')
   # Run massdns and save the output to a file
-  subprocess.run(["massdns", "-r", "/home/alvin/Tools/resolvers_trusted.txt", "-t", "A", "-o", "J", "-w", "massdns.json", "subdomains/subdomains.txt"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+  subprocess.run(["massdns", "-r", "/home/alvin/Tools/resolvers_trusted.txt", "-t", "A", "-o", "J", "-w", "massdns.json", "../subdomains/subdomains.txt"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
   # Extract the subdomains using jq
   subdomains = subprocess.run(["jq", 'select(.type=="A") | .name', "massdns.json"], capture_output=True).stdout.decode('utf-8')
@@ -110,9 +116,64 @@ def massdns_resolution():
   with open("resolved_subdomains.txt", "w") as f:
       for subdomain in subdomains:
           f.write(subdomain + "\n")
+  
+  os.chdir('..')
+
+def run_httpx():
+# Create a new folder for httpx
+  if not os.path.exists('httpx'):
+    os.makedirs('httpx')
+
+  # Change the working directory to the httpx folder
+  os.chdir('httpx')
+
+  # Run httpx to find live subdomains
+  subprocess.run(["httpx", "-l", "../massdns/resolved_subdomains.txt", "-p", "http:80", "-p", "https:443", "-p", "https:80", "-p", "http:81", "-p", "http:3000", "-p", "https:3000", "-p", "http:3001", "-p", "https:3001", "-p", "http:8000", "-p", "http:8080", "-p", "https:8080", "-p", "https:8443", "-o", "httpx.txt"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+  # Change the working directory to the target folder
+  os.chdir('..')
+
+
+def gowitness_screenshot():
+  # Create a new folder for screenshots
+  if not os.path.exists('gowitness'):
+    os.makedirs('gowitness')
+
+  # Change the working directory to the gowitness folder
+  os.chdir('gowitness')
+
+  # Run gowitness to take screenshots of the subdomains
+  subprocess.run(["gowitness", "file", "-f", "../httpx/httpx.txt"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+  # Change the working directory to the target folder
+  os.chdir('..')
+
+
+def web_crawling():
+  # Create a new folder for web crawling
+  if not os.path.exists('webcraw'):
+    os.makedirs('webcraw')
+
+  # Change the working directory to the webcraw folder
+  os.chdir('webcraw')
+
+  # Run Crawler, Waymore and common crawl
+  def crawler():
+    with open("../httpx/httpx.txt") as f:
+      for subdomain in f:
+        subdomain = subdomain.strip()
+        subprocess.run(["python3", "/home/alvin/bb/bin/crawler/crawler.py", "-d", subdomain, "-l", "2"])#, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["python3", "/home/alvin/bb/bin/waymore/waymore.py", "-d", subdomain, "-l", "2"])#, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    print('Crawler Done...\n')
+
+  crawler()
 
 
 # Python init function
 if __name__ == '__main__':
   # subdomain_enumeration()
-  massdns_resolution()
+  # massdns_resolution()
+  # run_httpx()
+  # gowitness_screenshot()
+  web_crawling()
